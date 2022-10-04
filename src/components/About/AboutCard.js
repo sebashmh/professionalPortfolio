68c56a448db84c881e7a1872e95354f219e56bb6
Sebastian Madrigal <sebas.hmh@gmail.com>
2022-10-04 17:10:50 -0400

    Minor changes aboutCard

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -43,12 +43,12 @@ function AboutCard() {
             </li>
             <li className="about-activity">
               <i className="far fa-hand-point-right"></i>
-              I enjoy folding paper, I have been practicing Origami for 10 years. I do not enjoy folding clothes though.
+              I enjoy  folding paper, I have been practicing Origami for 10 years. I do not enjoy folding clothes though.
             </li>
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
-            "If they say you are crazy, you are on the right track. "{" "}
+            "If they say   you are crazy, you are on the right track. "{" "}
           </p>
           <footer className="blockquote-footer">Sebastian Madrigal </footer>
         </blockquote>
